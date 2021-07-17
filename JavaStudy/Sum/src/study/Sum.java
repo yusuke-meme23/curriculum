656fa1e8eb325ec5342003e22a268965cc23e9df
yusuke-meme23 <82889927+yusuke-meme23@users.noreply.github.com>
2021-07-17 20:20:18 +0900

    2-4 課題提出

---
+++ JavaStudy/Sum/src/study/Sum.java
@@ -0,0 +1,26 @@
+package study;
+
+public class Sum {
+
+ 
+    public int plus(int a) {
+
+        return a + 1;
+
+    }
+
+ 
+    public int plus(int a, int b) {
+
+        return a + b;
+
+    }
+
+ 
+    public int plus(int a, int b, int c) {
+
+        return a + b + c;
+
+    }
+
+}
---
+++ JavaStudy/Sum/src/study/SumMain.java
@@ -0,0 +1,16 @@
+package study;
+
+public class SumMain {
+
+ 
+    public static void main(String[] args) {
+
+        Sum sum = new Sum();
+
+        int sumValue = sum.plus(1, 2 ,3);
+
+        System.out.println(sumValue);
+
+    }
+
+}
